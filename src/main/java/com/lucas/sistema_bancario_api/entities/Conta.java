6c6cb22bb44a825565ff7261aca22ce26f6b2837
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-18 23:57:43 -0300

    entidade Conta

---
+++ src/main/java/com/lucas/sistema_bancario_api/entities/Conta.java
@@ -0,0 +1,34 @@
+package com.lucas.sistema_bancario_api.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "contas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String cpf;
+
+    @NotNull
+    private String primeiroNome;
+
+    @NotNull
+    private String ultimoNome;
+
+    @Min(0)
+    private BigDecimal saldo;
+}
