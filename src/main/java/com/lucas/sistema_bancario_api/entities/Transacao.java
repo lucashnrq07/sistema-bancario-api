b4b842318573d081718cdeeaac6671a66d3ed23f
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-19 00:15:34 -0300

    Entidade Transacao

---
+++ src/main/java/com/lucas/sistema_bancario_api/entities/Transacao.java
@@ -0,0 +1,32 @@
+package com.lucas.sistema_bancario_api.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "transacoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "quem_enviou_id")
+    private Conta quemEnviou;
+
+    @ManyToOne
+    @JoinColumn(name = "quem_recebeu_id")
+    private Conta quemRecebeu;
+
+    @NotNull
+    private Instant instant;
+}
