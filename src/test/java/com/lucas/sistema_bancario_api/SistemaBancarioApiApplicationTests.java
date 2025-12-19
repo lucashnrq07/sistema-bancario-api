7ef6add682ca187cf74cc4b0cde8df0035cee488
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-18 23:57:14 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/sistema_bancario_api/SistemaBancarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.sistema_bancario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
