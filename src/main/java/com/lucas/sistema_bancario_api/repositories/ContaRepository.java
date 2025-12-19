a3ba4c51e44a4ce687fe4d1dbfa5c1bdf07796f2
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-19 00:06:26 -0300

    repositório Conta e configs do H2

---
+++ src/main/java/com/lucas/sistema_bancario_api/repositories/ContaRepository.java
@@ -0,0 +1,14 @@
+package com.lucas.sistema_bancario_api.repositories;
+
+import com.lucas.sistema_bancario_api.entities.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+
+    @Override
+    Optional<Conta> findById(Long id);
+
+    Optional<Conta> findContaByCpf(String cpf);
+}
