52abe0acedf9d1b470458ac2ac5675c156874553
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-19 16:45:56 -0300

    Formatando o momento na saída de operações

--- src/main/java/com/lucas/sistema_bancario_api/dtos/OperacaoRespostaDTO.java
+++ src/main/java/com/lucas/sistema_bancario_api/dtos/OperacaoRespostaDTO.java
@@ -1,7 +1,13 @@
 package com.lucas.sistema_bancario_api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record OperacaoRespostaDTO(String cpf, BigDecimal valor, LocalDateTime moment) {
+public record OperacaoRespostaDTO(
+        String cpf,
+        BigDecimal valor,
+        @JsonFormat(pattern = "dd/MM/yyyy - HH:mm:ss")
+        LocalDateTime moment) {
 }
--- src/main/java/com/lucas/sistema_bancario_api/dtos/TransferenciaRespostaDTO.java
+++ src/main/java/com/lucas/sistema_bancario_api/dtos/TransferenciaRespostaDTO.java
@@ -1,9 +1,15 @@
 package com.lucas.sistema_bancario_api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lucas.sistema_bancario_api.entities.Conta;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransferenciaRespostaDTO(String de, String para, BigDecimal valor, LocalDateTime momento){
+public record TransferenciaRespostaDTO(
+        String de,
+        String para,
+        BigDecimal valor,
+        @JsonFormat(pattern = "dd/MM/yyyy - HH:mm:ss")
+        LocalDateTime momento){
 }
